e0be62c4473e54f8535d2bdfdfeddf3e9e9ae205
idamc1992 <idan82@gmail.com>
2021-11-07 22:52:42 +0200

    change #2 - output of program for last name

--- Hello_Idan_Cohen.java
+++ Hello_Idan_Cohen.java
@@ -1,7 +1,7 @@
 public class Hello_Idan_Cohen {
     public static void main(String[] args) {
         for (int i=1; i<=10; i++) {
-            System.out.println("My first name is Idan ... " + i);
+            System.out.println("My last name is Cohen ... " + i);
         }
     }
 }

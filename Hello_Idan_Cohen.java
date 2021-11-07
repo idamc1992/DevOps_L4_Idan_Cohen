5bd363b40098f6a10a39aad4fa5ea6b1f0605c56
idamc1992 <idan82@gmail.com>
2021-11-07 22:57:42 +0200

    change #3 - output of program for ID number

--- Hello_Idan_Cohen.java
+++ Hello_Idan_Cohen.java
@@ -1,7 +1,5 @@
 public class Hello_Idan_Cohen {
     public static void main(String[] args) {
-        for (int i=1; i<=10; i++) {
-            System.out.println("My last name is Cohen ... " + i);
-        }
+        System.out.println("My ID number is 308060532 . ");
     }
 }

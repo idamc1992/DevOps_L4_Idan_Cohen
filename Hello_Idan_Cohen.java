10a57deda356c856d43973e6c520b0a6715fac28
idamc1992 <idan82@gmail.com>
2021-11-07 23:06:31 +0200

    change #5 - output of program for current month

--- Hello_Idan_Cohen.java
+++ Hello_Idan_Cohen.java
@@ -1,5 +1,5 @@
 public class Hello_Idan_Cohen {
     public static void main(String[] args) {
-        System.out.println("The current year is: 2021");
+        System.out.println("The current month is:  November");
     }
 }
